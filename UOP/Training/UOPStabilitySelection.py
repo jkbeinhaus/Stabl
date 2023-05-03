d22b862d9e952d2c92d2ad0d4cb6a373568e9d75
Jakob Franz Einhaus <jeinhaus@DN0a272189.SUNet>
2023-05-03 14:07:04 -0700

    odified pipeline

--- UOP/Training/UOPStabilitySelection.py
+++ UOP/Training/UOPStabilitySelection.py
@@ -1,6 +1,5 @@
 import warnings
 warnings.filterwarnings('ignore')
-
 # Libraries
 import numpy as np
 import pandas as pd
@@ -8,27 +7,27 @@ from pathlib import Path
 
 from sklearn.model_selection import RepeatedStratifiedKFold
 from sklearn.base import clone
+from sklearn.linear_model import Lasso
 
-from stabl.stabl import Stabl, plot_stabl_path, plot_fdr_graph, save_stabl_results
+from stabl.stabl import Stabl, plot_stabl_path, plot_fdr_graph, save_stabl_results, export_stabl_to_csv
 from stabl.preprocessing import LowInfoFilter, remove_low_info_samples
-
 
 from stabl.multi_omic_pipelines import multi_omic_stabl, multi_omic_stabl_cv, late_fusion_lasso_cv
 from stabl.single_omic_pipelines import single_omic_stabl, single_omic_stabl_cv
 from stabl.pipelines_utils import compute_features_table
 # Data
-X_dens = pd.read_csv('./UOPfinal_celldensities.csv',index_col=0)
-X_func = pd.read_csv('./UOPfinal_functional.csv',index_col=0)
-X_meta = pd.read_csv('./UOPfinal_metavariables.csv',index_col=0)
-X_neigh = pd.read_csv('./UOPfinal_neighborhood.csv',index_col=0)
+X_Celldensities = pd.read_csv('./UOPfinal_celldensities.csv',index_col=0)
+X_Function = pd.read_csv('./UOPfinal_functional.csv',index_col=0)
+X_Metavariables = pd.read_csv('./UOPfinal_metavariables.csv',index_col=0)
+X_Neighborhood = pd.read_csv('./UOPfinal_neighborhood.csv',index_col=0)
 
 y = pd.read_csv('./UOPfinal_outcome.csv',index_col=0)
 y = y.UOPfinal
 train_data_dict = {
-    "Celldensities": X_dens, 
-    "Function": X_func,
-    "Neighborhood": X_neigh,
-    "Metavariables":X_meta
+    "Celldensities": X_Celldensities, 
+    "Function": X_Function,
+    "Neighborhood": X_Neighborhood,
+    "Metavariables": X_Metavariables
     }
 # Results folder
 result_folder = "./Results UOPStabilitySelection"
@@ -39,7 +38,7 @@ for omic_name, X_omic in train_data_dict.items():
 stabl = Stabl(
     lambda_name='C',
     lambda_grid=np.linspace(0.01, 5, 10),
-    n_bootstraps=1000,
+    n_bootstraps=500,
     artificial_type="random_permutation",
     artificial_proportion=1.,
     replace=False,
@@ -61,9 +60,34 @@ predictions_dict = multi_omic_stabl_cv(
     task_type="binary",
     save_path=Path(result_folder)
 )
+# Multiomic Stabl
+lasso = Lasso(max_iter=int(1e6))
+
+stabl_multi = Stabl(
+    base_estimator=lasso,
+    lambda_name='alpha',
+    lambda_grid=np.logspace(0, 2, 30),
+    n_bootstraps=500,
+    artificial_proportion=1.,
+    artificial_type="random_permutation",
+    replace=False,
+    fdr_threshold_range=np.arange(0.2, 1, 0.01),
+    sample_fraction=.5,
+    random_state=42
+)
+
+stability_selection = clone(stabl_multi).set_params(artificial_type=None, hard_threshold=.3)
+predictions_dict = multi_omic_stabl(
+    data_dict=train_data_dict,
+    y=y,
+    stabl=stabl_multi,
+    stability_selection=stability_selection,
+    task_type="binary",
+    save_path=Path(result_folder, "omics"),
+)
 # Features Table
 selected_features_dict = dict()
-for model in ["STABL", "EF Lasso", "SS 03", "SS 05", "SS 08"]:
+for model in ["STABL", "SS 03"]:
     path = Path(result_folder, "Training-Validation", f"{model} coefficients.csv")
     try:
         selected_features_dict[model] = list(pd.read_csv(path, index_col=0).iloc[:, 0].index)
@@ -76,3 +100,5 @@ features_table = compute_features_table(
     task_type="binary"
 )
 features_table.to_csv(Path(result_folder, "Training-Validation", "Table of features.csv"))
+# Table for Maximum stability scores
+save_stabl_results(path = Path(result_folder, "Stabl-results"))
