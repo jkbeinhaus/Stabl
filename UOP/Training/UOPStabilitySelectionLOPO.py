2ab95b6a26a5b74192b70ec33e6dd9c29c88b1f8
Jakob Franz Einhaus <jeinhaus@DN52eh8r.SUNet>
2023-05-04 12:39:52 -0700

    LOPO

--- UOP/Training/UOPStabilitySelectionLOPO.py
+++ UOP/Training/UOPStabilitySelectionLOPO.py
@@ -30,7 +30,7 @@ train_data_dict = {
     "Neighborhood": X_Neighborhood,
     "Metavariables": X_Metavariables
     }
-patient_groups = pd.read_csv('./UOPfinal_patient_groups.csv', index_col=0)
+patient_groups = pd.read_csv('./UOPfinal_patient_groups.csv', index_col=0).to_numpy().flatten()
 # Results folder
 result_folder = "./Results UOPStabilitySelection"
 # Main script
@@ -61,7 +61,8 @@ predictions_dict = multi_omic_stabl_cv(
     stabl=stabl,
     stability_selection=stability_selection,
     task_type="binary",
-    save_path=Path(result_folder)
+    save_path=Path(result_folder,
+    outer_groups=patient_groups)
 )
 # Multiomic Training to derive coefficients
 np.random.seed(11)
