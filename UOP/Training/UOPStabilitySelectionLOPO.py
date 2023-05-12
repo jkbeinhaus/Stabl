33a7e5988e955f523799a26f791b997a9f942e78
Jakob Franz Einhaus <jeinhaus@RQYC2T0QFP-2.local>
2023-05-11 23:44:49 -0700

    validation

--- UOP/Training/UOPStabilitySelectionLOPO.py
+++ UOP/Training/UOPStabilitySelectionLOPO.py
@@ -23,14 +23,30 @@ X_Function = pd.read_csv('./UOPfinal_functional.csv',index_col=0)
 X_Metavariables = pd.read_csv('./UOPfinal_metavariables.csv',index_col=0)
 X_Neighborhood = pd.read_csv('./UOPfinal_neighborhood.csv',index_col=0)
 
+Val_Celldensities = pd.read_csv('../Validation/Val_celldensities.csv',index_col=0)
+Val_Function = pd.read_csv('../Validation/Val_functional.csv',index_col=0)
+Val_Metavariables = pd.read_csv('../Validation/Val_metavariables.csv',index_col=0)
+Val_Neighborhood = pd.read_csv('../Validation/Val_neighborhood.csv',index_col=0)
+
 y = pd.read_csv('./UOPfinal_outcome.csv',index_col=0)
 y = y.grade-1
+
+y_test = pd.read_csv('../Validation/Val_outcome.csv',index_col=0)
+y_test = y_test.grade-1
 train_data_dict = {
     "Celldensities": X_Celldensities, 
     "Function": X_Function,
     "Neighborhood": X_Neighborhood,
     "Metavariables": X_Metavariables
     }
+
+test_data_dict = {
+    "Celldensities": Val_Celldensities, 
+    "Function": Val_Function,
+    "Neighborhood": Val_Neighborhood,
+    "Metavariables": Val_Metavariables
+}
+# Define Leave-one-patient-out CV
 groups = pd.read_csv('./UOPfinal_patient_groups.csv', index_col=0)
 outer_groups = groups.to_numpy().flatten()
 # Results folder
@@ -48,15 +64,14 @@ stabl = Stabl(
     replace=False,
     fdr_threshold_range=np.arange(0.2, 1, 0.01),
     sample_fraction=.5,
-    random_state=11
+    random_state=1
  )
 
-outer_splitter = GroupShuffleSplit(n_splits=100, test_size=.2, random_state=11)
+outer_splitter = LeaveOneGroupOut()
 
-stability_selection = clone(stabl).set_params(artificial_type=None, hard_threshold=0.6)
+stability_selection = clone(stabl).set_params(artificial_type=None, hard_threshold=0.3)
 # Multi-omic Training-CV
-np.random.seed(11)
-
+np.random.seed(1)
 predictions_dict = multi_omic_stabl_cv(
     data_dict=train_data_dict,
     y=y,
@@ -67,7 +82,7 @@ predictions_dict = multi_omic_stabl_cv(
     save_path=Path(result_folder),
     outer_groups=outer_groups)
 # Multiomic Training to derive coefficients
-np.random.seed(11)
+np.random.seed(1)
 stabl_multi = Stabl(
     lambda_grid=np.linspace(0.01, 5, 30),
     n_bootstraps=5000,
@@ -77,17 +92,19 @@ stabl_multi = Stabl(
     replace=False,
     fdr_threshold_range=np.arange(0.2, 1, 0.01),
     sample_fraction=.5,
-    random_state=11
+    random_state=1
 )
 
-stability_selection = clone(stabl_multi).set_params(artificial_type=None, hard_threshold=.6)
+stability_selection = clone(stabl_multi).set_params(artificial_type=None, hard_threshold=.3)
 predictions_dict = multi_omic_stabl(
     data_dict=train_data_dict,
-    y=y,
+    y=y_test,
     stabl=stabl_multi,
     stability_selection=stability_selection,
     task_type="binary",
     save_path=Path(result_folder),
+    X_test=pd.concat(test_data_dict.values(),axis=1),
+    y_test=y_test
 )
 # Late fusion lasso
 late_fusion_lasso_cv(
@@ -110,6 +127,8 @@ features_table = compute_features_table(
     selected_features_dict,
     X_train=pd.concat(train_data_dict.values(), axis=1),
     y_train=y,
+    X_test=pd.concat(test_data_dict.values(), axis=1),
+    y_test=y_test,
     task_type="binary"
 )
 features_table.to_csv(Path(result_folder, "Training-Validation", "Table of features.csv"))
