2156c26859af52280bead9cde40f6a301b7736a1
Jakob Franz Einhaus <jeinhaus@DN52ehvm.SUNet>
2023-05-18 12:28:40 -0700

    batch

---
+++ UOP/Batchcorrected_UOPTrain_STATest /info.py
@@ -0,0 +1,7 @@
+Randomsplit:
+batch corrected using COMBAT, then UOP for Training, Stanford for Testing
+    
+artificial type:    random permutation
+var_threshold:      0.01
+bootstrap ratio:    0.5
+Seed:               111
