9e4a11703193a83219bc188a014bf16f905467bc
Jakob Franz Einhaus <jeinhaus@DN52ehvm.SUNet>
2023-05-18 09:30:22 -0700

    var

---
+++ UOP/RandomSplit_RP_3/info.py
@@ -0,0 +1,8 @@
+Randomsplit:
+UOP zscored, Stanford zscored
+then concatenated, randomly split with ratio 1:1 for institution and higher/lower grade
+    
+artificial type:    random_permutation
+var_threshold:      0.1
+bootstrap ratio:    0.5
+Seed:               11
