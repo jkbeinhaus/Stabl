83fa5ac3df971b61ac2f558575d805a20405a972
Jakob Franz Einhaus <jeinhaus@ANES-RQYC2T0QFP.fritz.box>
2023-05-29 21:58:20 +0200

    new

---
+++ UOP/RandomSplit_RP_Harmony_ZS/info.py
@@ -0,0 +1,8 @@
+Randomsplit:
+UOP and Stanford concatenated then batch corrected using Harmony
+then: randomly split with ratio 1:1 for institution and higher/lower grade, zscore on training applied to testing cohort
+    
+artificial type:    random_permutation
+var_threshold:      0.01
+bootstrap ratio:    0.5
+Seed:               111
