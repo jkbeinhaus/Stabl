ca2fe103c43860020d0f236cbc686100cbaba0a9
Jakob Franz Einhaus <jeinhaus@ANES-RQYC2T0QFP.fritz.box>
2023-05-31 09:53:17 +0200

    tests

---
+++ UOP/RandomSplit_RP_2_3/info.py
@@ -0,0 +1,9 @@
+Randomsplit:
+UOP zscored, Stanford zscored
+then concatenated, randomly split with ratio 1:1 for institution and higher/lower grade
+    
+artificial type:    random_permutation
+var_threshold:      0.01
+bootstrap ratio:    0.5
+Seed:               111
+train-test split:   80-20
---
+++ UOP/RandomSplit_RP_4_1/info.py
@@ -0,0 +1,8 @@
+Randomsplit:
+UOP zscored, Stanford zscored
+then concatenated, randomly split with ratio 1:1 for institution and higher/lower grade
+    
+artificial type:    random_permutation
+var_threshold:      0.01
+bootstrap ratio:    0.8
+Seed:               1
