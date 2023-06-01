cfa164a890a2b5246e73b75b4c86084b541fe5c3
Jakob Franz Einhaus <jeinhaus@RQYC2T0QFP-2.local>
2023-06-01 12:00:37 +0200

    Median_2_Mix

---
+++ UOP/RandomSplit_RP_Medians_2_Mix/info.py
@@ -0,0 +1,9 @@
+Randomsplit: MEDIANS DATAINPUT
+UOP zscored, Stanford zscored
+then concatenated, randomly split with ratio 1:1 for institution and higher/lower grade
+    
+artificial type:    random_permutation
+var_threshold:      0.01
+bootstrap ratio:    0.5, 0.3 for validation
+Seed:               111
+Train/Test:         60-40
