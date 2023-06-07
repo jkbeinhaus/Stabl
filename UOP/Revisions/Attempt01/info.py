a267fa3ece475fa734ac5f8462ae447315305223
Jakob Franz Einhaus <jeinhaus@RQYC2T0QFP-2.local>
2023-06-07 21:35:19 +0200

    patientlevel

---
+++ UOP/Revisions/Attempt01/info.py
@@ -0,0 +1,7 @@
+Datainput: Stanford2 for validation 
+UOP zscored then normalization applied to stanford cohort
+    
+artificial type:    random permutation
+var_threshold:      0.01
+bootstrap ratio:    0.5
+Seed:               111
