f6b85bd24eab5f3aa415ad4bda10d8bae2da83b5
Jakob Franz Einhaus <jeinhaus@RQYC2T0QFP-2.local>
2023-06-11 22:51:22 -0700

    reanalysis

---
+++ UOP/Revisions/Reanalysis02/info.py
@@ -0,0 +1,7 @@
+Datainput: Stanford2 for validation UOP Reanalysis
+UOP zscored then normalization applied to stanford cohort
+    
+artificial type:    random permutation
+var_threshold:      0.01
+bootstrap ratio:    0.7
+Seed:               111
