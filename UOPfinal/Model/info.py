a107ed858836e72ae1c356fce2c5fd8faf071122
Jakob Franz Einhaus <jeinhaus@DN52ehgl.SUNet>
2023-06-12 14:11:15 -0700

    final

---
+++ UOPfinal/Model/info.py
@@ -0,0 +1,8 @@
+Datainput: 
+DataUOP, DataSTA
+UOP cohort zscored, then normalization applied to STA cohort
+    
+artificial type:    random permutation
+var_threshold:      0.01
+bootstrap ratio:    0.5
+Seed:               111
